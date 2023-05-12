5234a38f24420ee56dc7de8639e04dc8af15e6c3
Giovanna Grangeiro <giigrangvanderlei1211@hotmail.com>
2023-05-12 14:16:32 -0300

    Projeto criado

---
+++ projetocp/src/test/java/com/giovannagrangeiro/projetocp/ProjetocpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovannagrangeiro.projetocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
